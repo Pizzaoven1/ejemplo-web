90be933df3e77ef8e41a2f5e9c1f11223c27cce1
Pizzaoven1 <54352585+Pizzaoven1@users.noreply.github.com>
2019-08-22 08:40:43 +0100

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,6 @@
+﻿function calc(){
+var argOne=document.getElementById('arg1').value;
+var argTwo=document.getElementById('arg2').value;
+var ans=(argOne*argTwo);
+document.getElementById('result').innerHTML=argOne + ' multiplicado por ' + argTwo + ' es igual a ' + ans;
+}
